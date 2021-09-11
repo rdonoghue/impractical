1c552ed3d6f7c6e4f0ee25cddbf4b0e0a2a9b103
rdonoghue <rdonoghue@gmail.com>
2021-09-11 15:37:18 -0400

    Adding some more files from the video

---
+++ flasksample.py
@@ -0,0 +1,7 @@
+import random
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Flask!"
---
+++ test_math.py
@@ -0,0 +1,11 @@
+def test_addition():
+    assert 2+2 == 4
+    assert 10 + 5 == 15
+
+def test_subtraction():
+    assert 4-2 == 2
+    assert 10-5 == 5
+
+def test_multiplication():
+    assert 2*2 == 4
+    assert 10*5 == 50
